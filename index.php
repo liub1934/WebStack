4a95d1e1e4d51b54d3fe194d0b4585df20c676e0
liub1934 <liub1934@gmail.com>
2020-12-14 16:00:58 +0800

    Del Fork me on GitHub

--- index.php
+++ index.php
@@ -29,7 +29,6 @@ include( 'templates/header-nav.php' );
         </li>
       </ul>
     </div>
-    <a href="https://github.com/owen0o0/WebStack" target="_blank"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
   </nav>
 <?php
 if(io_get_option('is_search')){include('search-tool.php'); }
